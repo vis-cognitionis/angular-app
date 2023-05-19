74c294f4efd1abfc1cc2f7537da67486a387bc97
Vis Cognitionis <omeralemdar94@gmail.com>
2023-05-19 15:54:37 +0300

    Add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { BlogCardsComponent } from './blog-cards/blog-cards.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, FeaturedComponent, BlogCardsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
